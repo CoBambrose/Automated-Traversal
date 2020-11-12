9865cec51874de4c7a7052d9b7c08960f2bd9aee
CoBambrose <cobaniaca@gmail.com>
2020-11-12 22:33:01 +0000

    NN visualisation

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -33,22 +33,77 @@ public class GUI {
 		
 		// display screen
 		pg.beginDraw();
+
+		pg.push();
 		
-		sketch.push();
 		pg.background(0,0,0,150);
 		pg.fill(255);
 		pg.textSize(28);
-		pg.translate(0, (sketch.height/2f - 224) / 2f - 10f);
-		pg.text("MSE: "+error, 20, 20, sketch.width, 40);
-		pg.text("EPOCH: "+epoch, 20, 20 + 32, sketch.width, 40);
-		pg.text("LOCATION: "+location, 20, 20 + 32*2, sketch.width, 40);
-		pg.text("    HEIGHT: "+height_, 20, 20 + 32*3, sketch.width, 40);
-		pg.text("    TEMPERATURE: "+temperature, 20, 20 + 32*4, sketch.width, 40);
-		pg.text("    DENSITY: "+density, 20, 20 + 32*5, sketch.width, 40);
-		pg.text("    STATE: "+state, 20, 20 + 32*6, sketch.width, 40);
-		sketch.pop();
+		pg.translate(0, (pg.height - 224) / 2f - 10f);
+		pg.text("MSE: "+error, 20, 20, pg.width, 40);
+		pg.text("EPOCH: "+epoch, 20, 20 + 32, pg.width, 40);
+		pg.text("LOCATION: "+location, 20, 20 + 32*2, pg.width, 40);
+		pg.text("    HEIGHT: "+height_, 20, 20 + 32*3, pg.width, 40);
+		pg.text("    TEMPERATURE: "+temperature, 20, 20 + 32*4, pg.width, 40);
+		pg.text("    DENSITY: "+density, 20, 20 + 32*5, pg.width, 40);
+		pg.text("    STATE: "+state, 20, 20 + 32*6, pg.width, 40);
+		
+		pg.pop();
+		pg.push();
+		
+		pg.fill(255);
+		pg.stroke(0);
+		pg.translate(pg.width/2f, 0);
+		pg.line(0,0,0,pg.height);
+		int inputs = 27;
+		int hiddens = 10;
+		int outputs = 8;
+		
+		for (int i = 0; i < inputs; i++) {
+			float x = (i+1f) * (pg.width/2f)/(inputs+1f);
+			float y = pg.height/5f;
+			pg.ellipse(x, y, 20, 20);
+			for (int j = 0; j < hiddens; j++) {
+				float x2 = (j+1f) * (pg.width/2f)/(hiddens+1f);
+				float y2 = 2f*(pg.height/5f);
+				pg.strokeWeight(.1f);
+				pg.stroke((float) (55f + vehicle.brain.weights[0].data[j][i]*200f));
+				pg.line(x,y,x2,y2);
+			}
+		}
+		for (int i = 0; i < hiddens; i++) {
+			float x = (i+1f) * (pg.width/2f)/(hiddens+1f);
+			float y = 2f*(pg.height/5f);
+			pg.ellipse(x,y,20,20);
+			for (int j = 0; j < hiddens; j++) {
+				float x2 = (j+1f) * (pg.width/2f)/(hiddens+1f);
+				float y2 = 3f*(pg.height/5f);
+				pg.strokeWeight(.1f);
+				pg.stroke((float) (55f + vehicle.brain.weights[1].data[j][i]*200f));
+				pg.line(x,y,x2,y2);
+			}
+		}
+		for (int i = 0; i < hiddens; i++) {
+			float x = (i+1f) * (pg.width/2f)/(hiddens+1f);
+			float y = 3f*(pg.height/5f);
+			pg.ellipse(x,y,20,20);
+			for (int j = 0; j < outputs; j++) {
+				float x2 = (j+1f) * (pg.width/2f)/(outputs+1f);
+				float y2 = 4f*(pg.height/5f);
+				pg.strokeWeight(.1f);
+				pg.stroke((float) (55f + vehicle.brain.weights[2].data[j][i]*200f));
+				pg.line(x,y,x2,y2);
+			}
+		}
+		for (int i = 0; i < outputs; i++) {
+			float x = (i+1f) * (pg.width/2f)/(outputs+1f);
+			float y = 4f*(pg.height/5f);
+			pg.ellipse(x,y,20,20);
+		}
+		
+		pg.pop();
 		
 		pg.endDraw();
-		sketch.image(pg.get(), 0, sketch.height/2f, sketch.width, sketch.height/2f);
+		sketch.image(pg.get(), 0, sketch.height/2f, pg.width, pg.height);
 	}
 }
